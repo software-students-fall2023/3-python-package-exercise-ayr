3b80e07f1eb97ce05ff13db8d610cefe190ebe08
YimengDuan2002 <123713674+YimengDuan2002@users.noreply.github.com>
2023-11-08 14:43:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bstpackage',
-    version='0.0.6',
+    version='0.0.7',
     packages=['bstpackage'],
     # Include other metadata like author, description, etc.
 )
